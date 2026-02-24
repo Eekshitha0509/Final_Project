a20fd3ea3240f9e2c16da9874c3493b67ffc00b0
Eekshitha Namala <eekshithanamala3@gmail.com>
2026-02-24 15:15:20 +0530

    Added complete project files

--- Frontend/js/profile.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("profileForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Profile saved successfully");
-
-    window.location.href = "home.html";
-});
