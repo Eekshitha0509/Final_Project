a1cc9f2e050a1895a6b81094514c8863e93b8986
Chandrashekar <your.email@gmail.com>
2026-02-25 00:48:59 +0530

    restore backend files

--- Frontend/hostel/src/App.jsx
+++ Frontend/hostel/src/App.jsx
@@ -1,36 +1,23 @@
-import React from "react";
-import Header from "./components/Header";
-import LandingPage from "./pages/LandingPage";
-import Profile from "./pages/Profile";
-import Homepage from "./pages/Homepage";
-import StudentLog from "./components/StudentLog";
-import MessPayment from "./pages/MessPayment";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import React from 'react'
+import Header from './components/Header'
+import LandingPage from './pages/LandingPage'
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import Profile from './pages/Profile'
+import Homepage from './pages/Homepage'
 function App() {
   return (
+    <>
+    <Header />
     <Router>
-
-      <Header />
-
       <Routes>
-
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/admin" element={<LandingPage />} />
-
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/homepage" element={<Homepage />} />
-
-        {/* ⭐ Fix login route */}
-        <Route path="/login" element={<StudentLog />} />
-
-        <Route path="/mess-payment" element={<MessPayment />} />
-
+        <Route path = '/' element = {<LandingPage/>}></Route>
+        <Route path = '/admin' element = {<LandingPage />}></Route>
+        <Route path = '/profile' element = {<Profile />}></Route>
+        <Route path= '/homepage' element={<Homepage />}></Route>
       </Routes>
-
     </Router>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
